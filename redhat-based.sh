8e62942958f84507cb31946235222b7b733d84df
Mikhail Zavoloka <mikhail.zavoloka@gmail.com>
2017-08-06 18:18:25 +0300

    Add some 3rd party rhel repos

--- redhat-based.sh
+++ redhat-based.sh
@@ -11,3 +11,24 @@ sudo yum install -y google-chrome-stable
 echo "Installing Skype..."
 sudo yum-config-manager --add-repo=https://repo.skype.com/data/skype-stable.repo
 sudo yum install skypeforlinux -y
+
+echo "Installing EPEL repository..."
+sudo yum install -y epel-release
+
+echo "Installing htop..."
+sudo yum install -y htop
+
+echo "Installing ntfs-3g..."
+sudo yum install -y ntfs-3g
+
+echo "Installing redshift..."
+sudo yum install -y redshift
+
+echo "Installing transmission..."
+sudo yum install -y transmission
+
+echo "Adding nux repo..."
+sudo rpm -Uvh http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-5.el7.nux.noarch.rpm
+
+echo "Installing vlc..."
+sudo yum install -y vlc
