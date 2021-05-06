a3135c1f7095383233d6d73d61cb0bf049c9d63c
Mikhail Zavoloka <mikhail.zavoloka@gmail.com>
2021-05-06 18:39:08 +0300

    More arch packages

--- arch-based.sh
+++ arch-based.sh
@@ -1,13 +1,12 @@
 # Upon following arch linux installation guide https://wiki.archlinux.org/index.php/Installation_guide
 # run this after arch-chrooting into /mnt:
 pacman -S        \
+    sudo         \
+    htop         \
     man-db       \
     dhclient     \
     inetutils    \
     net-tools    \
-    grub         \
-    refind       \ # configure it afterwars according to https://wiki.archlinux.org/index.php/REFInd
-    efibootmgr   \
     mlocate      \
     dhcp         \ # probably, not needed
     wget         \
@@ -20,3 +19,23 @@ pacman -S        \
     gvfs-mtp     \ # enable file transfer from mobile devices
     file-roller           \ # archive manager
     thunar-archive-plugin   # thunar's context menu for extracting archives
+
+# For booting you might need some of these packages:
+pacman -S        \
+    grub         \
+    refind       \ # configure it afterwars according to https://wiki.archlinux.org/index.php/REFInd
+    efibootmgr   \
+
+pacman -S        \
+    xorg         \
+    xfce4
+
+useradd --create-home mikhail
+passwd mikhail
+usermod -aG tty mikhail # to be able to run Xorg
+
+# add this line to /etc/sudoers:
+mikhail ALL=(ALL) NOPASSWD:ALL
+
+pacman -S        \
+    chromium
