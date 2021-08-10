6304c2365564363e1dd302390b14fd8cd0801de8
Mikhail Zavoloka <mikhail.zavoloka@gmail.com>
2021-08-11 01:47:08 +0300

    Fix path

--- civ5/disable-videos.sh
+++ civ5/disable-videos.sh
@@ -1,4 +1,4 @@
 # https://rtfm.co.ua/en/steam-civilization-v-arch-linux-and-errorinvalid-resolutions-constraints-0x0-must-not-be-greater-than-0x0/
-cd .local/share/Steam/steamapps/common/Sid\ Meier\'s\ Civilization\ V
+cd ~/.local/share/Steam/steamapps/common/Sid\ Meier\'s\ Civilization\ V
 for m in `find . -name \*.mov -type f`; do mv "$m" "$m"_bak; done
 find . -name *_bak
