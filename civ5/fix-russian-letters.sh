9a757eaa27caa60e6ce12ee94647ea964d0ce65b
Mikhail Zavoloka <mikhail.zavoloka@gmail.com>
2021-09-11 21:42:28 +0300

    upd

--- civ5/fix-russian-letters.sh
+++ civ5/fix-russian-letters.sh
@@ -1,4 +1,4 @@
 # http://www.civfanatics.ru/threads/11696
 
 mv ~/.steam/steam/steamapps/common/Sid\ Meier\'s\ Civilization\ V/steamassets/assets/gameplay/xml/newtext/english.xml{,.backup}
-cp english.xml ~/.steam/steam/steamapps/common/Sid\ Meier\'s\ Civilization\ V/steamassets/assets/gameplay/xml/newtext/english.xml
+cp civ5/english.xml ~/.steam/steam/steamapps/common/Sid\ Meier\'s\ Civilization\ V/steamassets/assets/gameplay/xml/newtext/english.xml
