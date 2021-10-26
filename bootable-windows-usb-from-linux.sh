5c46bfa02a2d1c24648ef7134de233d4b984b221
Mikhail Zavoloka <mikhail.zavoloka@gmail.com>
2021-10-26 13:01:28 +0300

    upd

--- bootable-windows-usb-from-linux.sh
+++ bootable-windows-usb-from-linux.sh
@@ -7,3 +7,4 @@ sudo mkfs.fat -F 32 /dev/sdc1 # won't fit if file size is >4GB
 sudo umount /mnt/iso     && sudo mount ~/Downlods/windows.iso /mnt/iso
 sudo umount /mnt/freshka && sudo mount /dev/sdc1 /mnt/freshka
 sudo cp -r /mnt/iso/* /mnt/freshka # Copy files from iso to new partition
+sudo umount /mnt/freshka
