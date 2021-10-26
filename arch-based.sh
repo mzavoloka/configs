4acf7a08aa2929bc4eecd29b7b91f74789a0d54f
Mikhail Zavoloka <mikhail.zavoloka@gmail.com>
2021-10-26 17:26:25 +0300

    Add unrar

--- arch-based.sh
+++ arch-based.sh
@@ -59,6 +59,7 @@ pacman -S               \
     file-roller         `# archive manager` \
     rofi                \
     mousepad            \
+    unrar               \
     xfce4-screenshooter \
     xfce4-screensaver   \
     thunar-archive-plugin   # thunar's context menu for extracting archives
@@ -186,3 +187,7 @@ fi
 # at autostart stage. To achieve this, put .desktop script to user's autostart folder:
 mkdir -p ~/.config/autostart/
 cp fix-nvidia-colors.desktop ~/.config/autostart/
+
+
+# Intel graphics;
+sudo pacman -S mesa lib32-mesa xf86-video-intel vulkan-intel
