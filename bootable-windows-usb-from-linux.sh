b62cf87aa80501a8be406f55e5247675df0f50aa
Mikhail Zavoloka <mikhail.zavoloka@gmail.com>
2021-10-26 12:59:14 +0300

    upd

--- bootable-windows-usb-from-linux.sh
+++ bootable-windows-usb-from-linux.sh
@@ -1,7 +1,9 @@
 sudo fdisk /dev/sdc
 g # create gpt partition table
 n # add new partition
-mkfs.fat -F 32 /dev/sdc1 # won't fit if file size is >4GB
-sudo mount ~/Downlods/windows.iso /mnt/iso
-sudo mount /dev/sdc1 /mnt/freshka
+t 11 # change partition type to 11 "Microsoft basic data"
+w # Save and exit
+sudo mkfs.fat -F 32 /dev/sdc1 # won't fit if file size is >4GB
+sudo umount /mnt/iso     && sudo mount ~/Downlods/windows.iso /mnt/iso
+sudo umount /mnt/freshka && sudo mount /dev/sdc1 /mnt/freshka
 sudo cp -r /mnt/iso/* /mnt/freshka # Copy files from iso to new partition
