7075eb66fa629152ff42fbfb5da0c26c3b0e24d2
Mikhail Zavoloka <mikhail.zavoloka@gmail.com>
2022-03-12 23:02:19 +0300

    Add w3m

--- arch-based.sh
+++ arch-based.sh
@@ -69,6 +69,7 @@ pacman -S               \
     xfce4-screenshooter \
     xfce4-screensaver   \
     ranger              \
+    w3m                 `# enables images in ranger` \
     thunar-archive-plugin   # thunar's context menu for extracting archives
 
 # Benchmarking
