a6a752dd1b59a374cd73667acedb4a0496df424b
Mikhail Zavoloka <mikhail.zavoloka@gmail.com>
2022-07-21 03:54:47 +0300

    Add note

--- arch-based.sh
+++ arch-based.sh
@@ -230,4 +230,4 @@ cp fix-nvidia-colors.desktop ~/.config/autostart/
 
 
 # Intel graphics;
-sudo pacman -S mesa lib32-mesa # xf86-video-intel vulkan-intel # these two udner question
+sudo pacman -S mesa lib32-mesa # xf86-video-intel vulkan-intel # these two udner question, glamorgl is loaded by Xorg instead, and it works fine
