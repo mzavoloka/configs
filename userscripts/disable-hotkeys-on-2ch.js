697ca2fc10749cf3a0591c6cd53b29891712c23a
Mikhail Zavoloka <mikhail.zavoloka@gmail.com>
2022-11-08 02:51:42 +0300

    Resolve hotkeys conflict on 2ch

---
+++ userscripts/disable-hotkeys-on-2ch.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name           Disable 2ch hotkeys
+// @namespace      Mikhail Zavoloka
+// @version        1.0
+// @description    Resolves d hotkey conflict with hotkeys of Video Speed Controller
+// @match          https://2ch.hk/*
+// @run-at         document-end
+// @grant          none
+// ==/UserScript==
+
+//['keydown', 'keyup'].forEach((eventName) => {
+['keyup'].forEach((eventName) => {
+  window.addEventListener(
+    eventName,
+    (e) => {
+      if (e.keyCode == 68) { // letter 'd'
+        e.stopPropagation();
+      }
+    },
+    true // capturing phase - very important
+  );
+});
