c598204916fe1d09e8a3d538a4e6b668f93b8940
Mikhail Zavoloka <mikhail.zavoloka@gmail.com>
2023-02-23 02:08:44 +0300

    Add direct smartlab external links extension

---
+++ userscripts/smart-lab-direct-external-links.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name           Smartlab direct external links
+// @namespace      Mikhail Zavoloka
+// @version        1.0
+// @description    Make smart-lab.ru links to lead directly to external website without warning
+// @match          https://smart-lab.ru/*
+// @run-at         document-end
+// @grant          none
+// ==/UserScript==
+
+var links = document.getElementsByTagName("a");
+
+for(var i=0, max=links.length; i<max; i++) {
+    var param_u = links[i].href
+        .replace(/.+\/r.php\?u=/g, '') // убрать всё до значения параметра 'u'
+        .replace(/&s=\d+$/g, '');      // убрать всё после значения параметра 'u'
+
+    links[i].href = decodeURIComponent( param_u ); // деэкранизация параметра 'u'
+}
